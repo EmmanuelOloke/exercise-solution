a54ea6866abec41e8b9379ff557ecb174262bdcd
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-05 18:57:59 +0100

    Added Solution 3

---
+++ solution3.js
@@ -0,0 +1,9 @@
+const teachingTeam = ['Leo', 'Kirti', 'PJ', 'Gemma', 'James', 'Hello', 'P'];
+
+let longestName = '';
+
+for (member of teachingTeam) {
+    if (member.length > longestName.length)
+        longestName = member;
+}
+console.log(longestName);

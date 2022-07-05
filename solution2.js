c98b022ee150dc46cdc47176af986956911d38e4
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-05 18:46:17 +0100

    Add solution to question 2

---
+++ solution2.js
@@ -0,0 +1,6 @@
+const teachingTeam = ['Leo', 'Kirti', 'PJ', 'Gemma', 'James', 'Hello', 'P'];
+
+for (let i = 0; i < teachingTeam.length; i++) {
+    teachingTeam[i] = teachingTeam[i].toUpperCase();
+}
+console.log(teachingTeam);
